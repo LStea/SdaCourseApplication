05dae07cdcfa74da764b340d36851a66e4ca5c97
Leszek <ziege4@gmail.com>
2017-02-25 15:51:34 +0100

    SpeedTest i Milioners applications

---
+++ app/src/main/java/com/example/rent/myapplication/Milioners/SingleAnswer.java
@@ -0,0 +1,11 @@
+package com.example.rent.myapplication.Milioners;
+
+/**
+ * Created by RENT on 2017-02-25.
+ */
+public class SingleAnswer {
+
+    private String text;
+    private boolean isCorrect;
+
+}
